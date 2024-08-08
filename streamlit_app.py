25bc68e547cdaa7a902f1c845c380bc7aedd2085
$h0gunX <harshilpatel1973@gmail.com>
2024-08-08 16:45:02 +0530

    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,54 @@
+from llama_index.core.response.notebook_utils import display_source_node
+from llama_index.core.retrievers import QueryFusionRetriever
+from llama_index.retrievers.bm25 import BM25Retriever
+import nest_asyncio
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core import VectorStoreIndex, StorageContext
+import sys
 import streamlit as st
-from openai import OpenAI
+import os
+__import__('pysqlite3')
+nest_asyncio.apply()
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+db = chromadb.PersistentClient(path="./legal_doc_hybrid_v2")
+chroma_collection = db.get_or_create_collection("dense_vectors")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+docstore = SimpleDocumentStore.from_persist_path("docstore.json")
+
+storage_context = StorageContext.from_defaults(
+    docstore=docstore, vector_store=vector_store
+)
+index = VectorStoreIndex(nodes=[], storage_context=storage_context)
+
+retriever = QueryFusionRetriever(
+    [
+        index.as_retriever(similarity_top_k=5),
+        BM25Retriever.from_defaults(
+            docstore=index.docstore, similarity_top_k=5
+        ),
+    ],
+    num_queries=1,
+    use_async=True,
+    retriever_weights=[0.4, 0.6],
+    similarity_top_k=5,
+    mode="relative_score",
+    verbose=True,
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# Show title and description.
+st.title("Legal Documents Hybrid Search")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+os.environ["HF_TOKEN"] = st.secret["HF_TOKEN"]
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+search = st.text_input("Search through documents by keyword", value="")
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+if st.button("Search"):
+    nodes = retriever.retrieve("bail application  ")
+    for node in nodes:
+        st.rrite(node.metadata['file_name'])
+        # print("---")
+        st.markdown(display_source_node(node, source_length=5000))
