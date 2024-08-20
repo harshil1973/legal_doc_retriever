b7143a9917cb4650fab8932176aa8af241ddce07
$h0gunX <harshilpatel1973@gmail.com>
2024-08-20 13:07:42 +0530

    this is it

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,6 @@ if search_btn:
     nodes = retriever.retrieve(search)
     for node in nodes:
         st.write(node.metadata['file_name'])
-        st.write("---")
         st.write("Node-id = " + node.node_id)
         st.write("Text = " +node.text)
+        st.write("---")
